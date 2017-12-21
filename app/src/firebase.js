ad7cb2853718542cee8e4c52569938791a409c82
nahum zsilva <nahum_zsilva@acl.com>
2017-12-21 00:09:39 -0800

    🍺  adjust readme file

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -3,8 +3,4 @@ import config from  './.firebase.config.json';
 
 firebase.initializeApp(config);
 
-// export const provider = new firebase.auth.GoogleAuthProvider();
-//
-// export const auth = firebase.auth();
-
 export default firebase;
